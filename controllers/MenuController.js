4a812a4ca166a190f7279ac838f95d0832d85b6d
Tyler <tylerpoore2000@gmail.com>
2019-06-27 22:25:45 -0700

    Pushing assignment work again

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
                 message: "Please choose from an option from below: ",
                 choices: [
                     "Add new contact",
+                    "Date",
                     "Exit"
                 ]
             }
@@ -22,6 +23,9 @@ module.exports = class MenuController {
                 case "Add new contact":
                     this.addContact();
                     break;
+                case "Date":
+                    this.getDate();
+                    break;
                 case "Exit":
                     this.exit();
                 default:
@@ -34,6 +38,7 @@ module.exports = class MenuController {
     }
 
     clear(){
+        // Useful to know
         console.log("Clearing the screen! ");
         console.log("\x1Bc");
     }
@@ -41,6 +46,21 @@ module.exports = class MenuController {
     addContact(){
         this.clear();
         console.log("addContact called ");
+        this.main();
+    }
+
+    getDate() {
+        // Clears the screen
+        this.clear();
+
+        // Declares and sets the date variables to be equal to the current date time I actually wrote this code back in december for my work :)
+        let dt = new Date(),
+            result = (dt.getFullYear() + "/" + (''+(dt.getMonth()+1)).padStart(2,'0') + ('/'+dt.getDate()).padStart(2,'0') + " " + (''+dt.getHours()).padStart(2,'0') + ":" + (''+dt.getMinutes()).padStart(2,'0'));
+
+        // Logs the result date time variable
+        console.log("The current date and time is: " + result);
+
+        // Calls main method to continue program
         this.main();
     }
 
